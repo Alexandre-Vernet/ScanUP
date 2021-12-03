130c1b116ca82045a8af2b95e86c0f3f6f925953
Emma974 <emma1997@wanadoo.fr>
2021-12-03 11:51:15 +0100

    onint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../../config';
 @Component({
@@ -6,9 +6,18 @@ import { firebaseConfig } from '../../config';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit() {
   title = 'ScanUP';
 
   // Initialize Firebase
   app = initializeApp(firebaseConfig);
+  OnInit() {
+    // Scanner un produit
+    // Modifer la quantité
+    // Supprimer produit au scan
+    // Ajouter un produit au code
+    //Payer d'un coup
+    //Payer en plusieurs fois
+    //Payer en espèce
+  }
 }
