9c97545408c8ca751d1f35757b9275f531608f47
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2021-12-03 11:38:10 +0100

    Remove firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '../../config';
+import { Component } from "@angular/core";
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ScanUP';
+    title = "ScanUP";
 
-  // Initialize Firebase
-  app = initializeApp(firebaseConfig);
 }
