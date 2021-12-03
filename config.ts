5c099c3fb83aa32ec4922546c39f3d45cb89f885
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2021-12-03 08:42:56 +0100

    Install firebase & bootstrap

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDbyPX6ERGSEWnVGTkpotYrrGrU4nHgH0E',
+  authDomain: 'scanup-504d6.firebaseapp.com',
+  projectId: 'scanup-504d6',
+  storageBucket: 'scanup-504d6.appspot.com',
+  messagingSenderId: '891641232555',
+  appId: '1:891641232555:web:c791f839e5a0d68fcf0bee',
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../../config';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ScanUP';
+
+  // Initialize Firebase
+  app = initializeApp(firebaseConfig);
 }
