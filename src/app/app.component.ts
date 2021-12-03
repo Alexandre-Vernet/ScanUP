91d295a97f3d4804121a825b4868b2a82aa58640
Emma974 <emma1997@wanadoo.fr>
2021-12-03 12:06:50 +0100

    scenar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'ScanUP';
+    title = 'ScanUP';
 
-  ngOnInit() {
-    // Scanner un produit
-    // Modifer la quantité
-    // Supprimer produit au scan
-    // Ajouter un produit au code
-    //Payer d'un coup
-    //Payer en plusieurs fois
-    //Payer en espèce
-  }
+    products = [];
+
+    ngOnInit() {
+        // Scanner un produit
+        // Modifer la quantité
+        // Scanner un autre produit
+        // Scanner un autre produit
+        // Supprimer produit au scan
+        // Ajouter un produit au code
+        //Payer en plusieurs fois
+        //Payer un partie CB
+        //Payer le reste en espèce
+    }
+    ScanProduct(product: any) {
+        this.products.push(product);
+    }
 }
