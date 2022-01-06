bbefe5dd280a99dc8babcefcd55057186c197aa9
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-01-06 16:15:38 +0100

    Add new colors

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { StateService } from '../service/state.service';
+import { Component, OnInit } from "@angular/core";
+import { StateService } from "../service/state.service";
 import * as moment from "moment";
 
 @Component({
-    selector: 'app-nav',
-    templateUrl: './nav.component.html',
-    styleUrls: ['./nav.component.scss'],
+    selector: "app-nav",
+    templateUrl: "./nav.component.html",
+    styleUrls: ["./nav.component.scss"]
 })
 export class NavComponent implements OnInit {
     currentState: string;
@@ -20,23 +20,23 @@ export class NavComponent implements OnInit {
     ngOnInit(): void {
         setInterval(() => {
             let now = new Date();
-            this.myDate = moment(now).locale('fr').format('dddd DD/MM HH:mm');
+            this.myDate = moment(now).locale("fr").format("dddd DD/MM HH:mm");
         }, 1000);
     }
 
     colorGreen() {
-        document.body.style.backgroundColor = 'green';
+        document.body.style.backgroundColor = "green";
     }
 
     colorSalmon() {
-        document.body.style.backgroundColor = '#FA8072';
+        document.body.style.backgroundColor = "#FA8072";
     }
 
     colorBlack() {
-        document.body.style.backgroundColor = 'black';
+        document.body.style.backgroundColor = "black";
     }
 
     colorDefault() {
-        document.body.style.backgroundColor = 'rgb(75 85 95 / 75%)';
+        document.body.style.backgroundColor = "white";
     }
 }
