9c99e6bead856b506629b3dab0cf6c42c7420e44
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-01-06 11:12:22 +0100

    Display modal confirm delete product

--- src/app/list-products/list-products.component.ts
+++ src/app/list-products/list-products.component.ts
@@ -1,41 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Cart } from '../cart';
-import { CartService } from '../service/cart.service';
-import { StateService } from '../service/state.service';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Cart } from "../cart";
+import { CartService } from "../service/cart.service";
+import { StateService } from "../service/state.service";
 
 @Component({
-    selector: 'app-list-products',
-    templateUrl: './list-products.component.html',
-    styleUrls: ['./list-products.component.scss'],
+    selector: "app-list-products",
+    templateUrl: "./list-products.component.html",
+    styleUrls: ["./list-products.component.scss"]
 })
 export class ListProductsComponent implements OnInit {
     cart: Cart = new Cart();
+    productId: number;
     productList = [
         {
             id: 1,
-            name: 'Tronçonneuse électrique RCS2340B2C 2300W 40cm - RYOBI',
+            name: "Tronçonneuse électrique RCS2340B2C 2300W 40cm - RYOBI",
             price: 99.9,
-            quantity: 2,
+            quantity: 2
         },
         {
             id: 2,
-            name: 'Ratelier de 9 clés torx 1,5 à 10mm chrome vanadium - INVENTIV',
+            name: "Ratelier de 9 clés torx 1,5 à 10mm chrome vanadium - INVENTIV",
             price: 12.5,
-            quantity: 2,
+            quantity: 2
         },
         {
             id: 3,
-            name: 'Rénovateur brillant Star longue durée tous sols intérieurs Starwax 1L',
+            name: "Rénovateur brillant Star longue durée tous sols intérieurs Starwax 1L",
             price: 17.95,
-            quantity: 2,
+            quantity: 2
         },
         {
             id: 4,
-            name: 'Peinture sol trafic extrême satin 0.5L - Rouge brique - V33',
+            name: "Peinture sol trafic extrême satin 0.5L - Rouge brique - V33",
             price: 24.9,
-            quantity: 2,
-        },
+            quantity: 2
+        }
     ];
+
+    @ViewChild("closeModalConfirmDeleteProduct") closeModalConfirmDeleteProduct: ElementRef;
+
     constructor(
         private cartService: CartService,
         private stateService: StateService
@@ -45,17 +49,25 @@ export class ListProductsComponent implements OnInit {
         });
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+    }
+
     deleteProduct(id) {
         this.cartService.deleteProduct(id);
     }
+
     editMode(id) {
         this.stateService.checkState(
-            'waitScan',
-            'edit',
+            "waitScan",
+            "edit",
             true,
             this.stateService.setIdEdit(id)
             //+ transforme td en input
         );
+    }
+
+    getProductId(productId) {
+        this.productId = productId;
+        this.closeModalConfirmDeleteProduct.nativeElement.click();
     }
 }
