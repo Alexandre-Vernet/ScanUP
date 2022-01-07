84288d28f290c2d85142a79c157e06eb8d8a8aa1
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2022-01-07 10:10:22 +0100

    cursor pointer over keyboard auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -50,11 +50,8 @@ export class AuthComponent implements OnInit {
         });
     }
 
-    clearForm() {
-        this.codeControl.setValue("");
-    }
     clear() {
         this.codeControl.setValue(null);
-        this.valueClavier = '';
+        this.valueClavier = "";
     }
 }
