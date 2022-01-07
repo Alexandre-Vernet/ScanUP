a106e83b17ab5704082e1d2a4be43c631298b82a
Emma974 <emma1997@wanadoo.fr>
2022-01-07 12:10:18 +0100

    ajout photo articles

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { Router } from "@angular/router";
-import { CartService } from "../service/cart.service";
-import { StateService } from "../service/state.service";
-import { AuthService } from "../service/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CartService } from '../service/cart.service';
+import { StateService } from '../service/state.service';
+import { AuthService } from '../service/auth.service';
 
 @Component({
-    selector: "app-auth",
-    templateUrl: "./auth.component.html",
-    styleUrls: ["./auth.component.scss"]
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
     currentState: string;
-    status = "espece";
+    status = 'espece';
     totalPrice = 100;
     codeControl = new FormControl();
     enterQte = 12;
-    valueClavier = "";
+    valueClavier = '';
     loginOK = true;
 
     constructor(
@@ -24,11 +24,9 @@ export class AuthComponent implements OnInit {
         private cartService: CartService,
         private router: Router,
         private auth: AuthService
-    ) {
-    }
+    ) {}
 
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 
     clavierNumber(number) {
         this.valueClavier += number;
@@ -43,7 +41,7 @@ export class AuthComponent implements OnInit {
         this.auth.login(code.value).then(async (auth) => {
             if (auth) {
                 this.loginOK = true;
-                await this.router.navigateByUrl("/general");
+                await this.router.navigateByUrl('/general');
             } else {
                 this.loginOK = false;
             }
@@ -52,6 +50,6 @@ export class AuthComponent implements OnInit {
 
     clear() {
         this.codeControl.setValue(null);
-        this.valueClavier = "";
+        this.valueClavier = '';
     }
 }
--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -1,45 +1,49 @@
 import { Injectable } from '@angular/core';
 
 const PRODUCT_LIST = [
-  {
-    id: 1,
-    name: 'Tronçonneuse électrique RCS2340B2C 2300W 40cm - RYOBI',
-    price: 99.00,
-    quantity: 1,
-  },
-  {
-    id: 2,
-    name: 'Ratelier de 9 clés torx 1,5 à 10mm chrome vanadium - INVENTIV',
-    price: 12.5,
-    quantity: 1,
-  },
-  {
-    id: 3,
-    name: 'Rénovateur brillant Star longue durée tous sols intérieurs Starwax 1L',
-    price: 17.95,
-    quantity: 1,
-  },
-  {
-    id: 4,
-    name: 'Peinture sol trafic extrême satin 0.5L - Rouge brique - V33',
-    price: 24.00,
-    quantity: 1,
-  },
+    {
+        id: 1,
+        name: 'Tronçonneuse électrique RCS2340B2C 2300W 40cm - RYOBI',
+        price: 99.0,
+        quantity: 1,
+        img: 'tronconneuse.png',
+    },
+    {
+        id: 2,
+        name: 'Ratelier de 9 clés torx 1,5 à 10mm chrome vanadium - INVENTIV',
+        price: 12.5,
+        quantity: 1,
+        img: 'ratelier.png',
+    },
+    {
+        id: 3,
+        name: 'Rénovateur brillant Star longue durée tous sols intérieurs Starwax 1L',
+        price: 17.95,
+        quantity: 1,
+        img: 'hammer.png',
+    },
+    {
+        id: 4,
+        name: 'Peinture sol trafic extrême satin 0.5L - Rouge brique - V33',
+        price: 24.0,
+        quantity: 1,
+        img: 'paint.jpg',
+    },
 ];
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ProductsService {
-  // "base de données"
+    // "base de données"
 
-  constructor() { }
+    constructor() {}
 
-  checkProductExist(id: number) {
-    return {...PRODUCT_LIST.find((product) => product.id === id)};
-  }
+    checkProductExist(id: number) {
+        return { ...PRODUCT_LIST.find((product) => product.id === id) };
+    }
 
-  productList(){
-    return [...PRODUCT_LIST];
-  }
+    productList() {
+        return [...PRODUCT_LIST];
+    }
 }
